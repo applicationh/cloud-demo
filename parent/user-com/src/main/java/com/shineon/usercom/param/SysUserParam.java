79a1773dc6ab4149faeed24f1f2e5577a7d246ff
wsh <986777430@qq.com>
2020-04-22 11:40:11 +0800

    页码查询

--- parent/user-com/src/main/java/com/shineon/usercom/param/SysUserParam.java
+++ parent/user-com/src/main/java/com/shineon/usercom/param/SysUserParam.java
@@ -20,7 +20,16 @@ public class SysUserParam implements Serializable {
     private Integer page=1;
     
     private Integer limit=10;
-    
+    private String  username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public Integer getId() {
         return id;
     }
